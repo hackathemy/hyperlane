b7238a19e7dfac71f5cb2470bd0c7a1086afeb1c
bagjinhwan <jinhwan.park@vogoplay.com>
2024-05-17 00:55:59 +0900

    add try catch

--- routes/hyperlane.js
+++ routes/hyperlane.js
@@ -36,48 +36,54 @@ const CALDERA = {
 router.post('/bridge', async function(req, res, next) {
   let {chain, recipient, amount } = req.body;
 
-  amount = ethers.utils.parseEther(amount.toString())
-  recipient = recipient.trim();
+  try {
+    amount = ethers.utils.parseEther(amount.toString())
+    recipient = recipient.trim();
 
-  if(recipient.length !== 42) {
-    res.status(500).send('잘못된 recipient 주소입니다.');
-    return;
-  }
-
-  let fileCommand = '';
-  let chainCommand=''
-
-  if (chain === MODE.NAME) {
-    fileCommand = MODE.FILE
-    chainCommand = MODE.CHAIN
-  } else if (chain === GELATO.NAME) {
-    fileCommand = GELATO.FILE
-    chainCommand = GELATO.CHAIN
-  } else if (chain === CALDERA.NAME) {
-    fileCommand = CALDERA.FILE
-    chainCommand = CALDERA.CHAIN
-  } else {
-    res.status(500).send('잘못된 chain 이름입니다.');
-    return;
-  }
-
-  let command = `hyperlane send transfer --warp deployments/warp_routes/BB/${fileCommand} --wei ${amount} --key ${KEY} --origin ${chainCommand} --destination sepolia --recipient ${recipient}`;
-  console.log('command', command);
-  console.log('command start')
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`에러 발생: ${error.message}`);
-      res.status(500).send('서버에서 명령어 실행 중 에러가 발생했습니다.');
+    if(recipient.length !== 42) {
+      res.status(500).send('잘못된 recipient 주소입니다.');
       return;
     }
-    if (stderr) {
-      console.error(`명령어 실행 중 에러: ${stderr}`);
-      res.status(500).send('서버에서 명령어 실행 중 에러가 발생했습니다.');
+
+    let fileCommand = '';
+    let chainCommand=''
+
+    if (chain === MODE.NAME) {
+      fileCommand = MODE.FILE
+      chainCommand = MODE.CHAIN
+    } else if (chain === GELATO.NAME) {
+      fileCommand = GELATO.FILE
+      chainCommand = GELATO.CHAIN
+    } else if (chain === CALDERA.NAME) {
+      fileCommand = CALDERA.FILE
+      chainCommand = CALDERA.CHAIN
+    } else {
+      res.status(500).send('잘못된 chain 이름입니다.');
       return;
     }
-    console.log(`명령어 실행 결과: ${stdout}`);
-    res.send(stdout);
-  });
+
+    let command = `hyperlane send transfer --warp deployments/warp_routes/BB/${fileCommand} --wei ${amount} --key ${KEY} --origin ${chainCommand} --destination sepolia --recipient ${recipient}`;
+    console.log('command', command);
+    console.log('command start')
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`에러 발생: ${error.message}`);
+        res.status(500).send('서버에서 명령어 실행 중 에러가 발생했습니다.');
+        return;
+      }
+      if (stderr) {
+        console.error(`명령어 실행 중 에러: ${stderr}`);
+        res.status(500).send('서버에서 명령어 실행 중 에러가 발생했습니다.');
+        return;
+      }
+      console.log(`명령어 실행 결과: ${stdout}`);
+      res.send(stdout);
+    });
+  }catch (error){
+    console.error('에러 발생:', error);
+    res.status(500).json({ error: '서버에서 오류가 발생했습니다.' });
+  }
+
 });
 router.post('/transfer', async function(req, res, next) {
    try {
